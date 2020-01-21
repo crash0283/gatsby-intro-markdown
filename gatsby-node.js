58631f096762ec749c5afa9b9989d47e1ccf38a8
crash0283 <rasch83@gmail.com>
2020-01-21 08:15:05 -0600

    init

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,60 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+//Using onCreateNode to create the path or "slug" to our page
+//This onCreateNode function will be called by Gatsby whenever a new node is created (or updated).
+//To see changes, you must stop and restart server
+
+//The function handles finding the parent File node along with creating the slug.
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require("path")
+
+exports.onCreateNode = ({ node, getNode, actions  }) => {
+    const { createNodeField } = actions
+    if (node.internal.type === `MarkdownRemark`) {
+        // const fileNode = getNode(node.parent)
+        const slug = createFilePath({ node, getNode, basePath: `data` })
+
+        //Now you can add your new slugs directly onto the MarkdownRemark nodes. This is powerful, as any data you add to nodes is available to query later with GraphQL. 
+        //This function allows you to create additional fields on nodes created by other plugins. 
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+          })
+      }
+  }
+
+//added an implementation of the createPages API which Gatsby calls so plugins can add pages.
+
+//You need one additional thing beyond a slug to create pages: a page template component. Like everything in Gatsby, programmatic pages are powered by React components. When creating a page, you need to specify which component to use.
+  exports.createPages = async ({ graphql, actions }) => {
+    // **Note:** The graphql function call returns a Promise
+    // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+
+    const { createPage } = actions
+    const result = await graphql(`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `)
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/post.js`),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
+        })
+      })
+  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,52 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import { graphql } from "gatsby"
+//Add Link to link to programatically created pages
+import { graphql, Link } from "gatsby"
 
+//add 'fields -> slug' to the graph
+//remove 'html'
 export const query = graphql`
-{
-  allMarkdownRemark(sort: {fields: frontmatter___data, order: DESC}) {
-    edges {
-      node {
-        html
-        frontmatter {
-          title
-          data(formatString: "MMMM Do YYYY")
+  {
+    allMarkdownRemark(sort: { fields: frontmatter___data, order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            data(formatString: "MMMM Do YYYY")
+          }
+          excerpt
         }
       }
     }
   }
-}
 `
 
-const IndexPage = ({data}) => {
-  
-  return (<Layout>
-    <SEO title="Home" />
-    <div>
-    {
-      data.allMarkdownRemark.edges.map(({node}) => (
-        <>
-        <h1>{node.frontmatter.title}</h1>
-        <h4>{node.frontmatter.data}</h4>
-        <div dangerouslySetInnerHTML={{__html: node.html}}></div>
-        <hr style={{height: "3px"}} />
-        </>
-      ))
-    }
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+//add Link and link to 'node.fields.slug' to link to correct path
+//also we added 'excerpt' and removed 'html'
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <>
+            <Link to={node.fields.slug}>
+              <h1>{node.frontmatter.title}</h1>
+            </Link>
+            <h4>{node.frontmatter.data}</h4>
+            <p>{node.excerpt}</p>
+            <hr style={{ height: "3px" }} />
+          </>
+        ))}
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
   )
 }
 
